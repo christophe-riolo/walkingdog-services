e8e487b4bbf7a3ef6f5b89f78c7675cee6e4bcb6
Paolo Escobar <pao.esco@outlook.com>
2016-10-10 10:46:22 +0100

    Login page segment bugfix. Error handler login failed

--- walkingdog-mobile/src/pages/login/login.ts
+++ walkingdog-mobile/src/pages/login/login.ts
@@ -15,83 +15,86 @@ import { HomePage } from '../home/home';
   Ionic pages and navigation.
   */
   @Component({
-      selector: 'page-login',
-      templateUrl: 'login.html'
+    selector: 'page-login',
+    templateUrl: 'login.html'
   })
   export class LoginPage {
 
-      segments: string;
-      loader: any;
-      loginForm: FormGroup;
-      signupForm: FormGroup;
+    segments: string;
+    loader: any;
+    loginForm: FormGroup;
+    signupForm: FormGroup;
 
-      constructor(
-          public auth: Auth, 
-          public user: User, 
-          public loadingCtrl: LoadingController,
-          public navCtrl: NavController,
-          fb: FormBuilder) {
+    constructor(
+      public auth: Auth, 
+      public user: User, 
+      public loadingCtrl: LoadingController,
+      public navCtrl: NavController,
+      fb: FormBuilder) {
 
-          if (this.auth.isAuthenticated()) {
-              this.navCtrl.setRoot(HomePage);
+      if (this.auth.isAuthenticated()) {
+        this.navCtrl.setRoot(HomePage);
+      }
+
+      this.segments = 'login';
+      this.loader = this.loadingCtrl.create({
+        content: "Please wait..."
+      });
+
+      this.loginForm = fb.group({
+        'email': [''],
+        'password': ['']
+      });
+
+      this.signupForm = fb.group({
+        'email': [''],
+        'password': ['']
+      });
+    }
+
+    ionViewDidLoad() {
+      console.log('Hello Login Page');
+    }
+
+    signup(value: string) {
+      this.loader.present();
+      console.log(`Submitted values : email = ${value.email}, password = ${value.password}`);
+      let details: UserDetails = {'email': value.email, 'password': value.password};
+      this.auth.signup(details).then(() => {
+        alert('Signed up')
+        this.segments = 'login';
+        this.loader.dismiss();
+      }, (err: IDetailedError<string[]>) => {
+        for (let e of err.details) {
+          if (e === 'conflict_email') {
+            alert('Email already exists.');
+          } else {
+            alert(e);
           }
+        }
+        this.loader.dismiss();
+      });
+    }
 
-          this.segments = 'login';
-          this.loader = this.loadingCtrl.create({
-              content: "Please wait..."
-          });
+    login(value: string) {
+      this.loader.present();
+      console.log(`Submitted values : email = ${value.email}, password = ${value.password}`);
+      let details: UserDetails = {'email': value.email, 'password': value.password};
+      this.auth.login('basic', details).then( () =>{
+        this.loader.dismiss();
+        this.navCtrl.setRoot(HomePage);
+      }, (err) => {
+        this.loader.dismiss();
+        alert(err);
+      });
 
-          this.loginForm = fb.group({
-            'email': [''],
-            'password': ['']
-          });
+    }
 
-          this.signupForm = fb.group({
-            'email': [''],
-            'password': ['']
-          });
-      }
-
-      ionViewDidLoad() {
-          console.log('Hello Login Page');
-      }
-
-      signup(value: string) {
-          this.loader.present();
-          console.log(`Submitted values : email = ${value.email}, password = ${value.password}`);
-          let details: UserDetails = {'email': value.email, 'password': value.password};
-          this.auth.signup(details).then(() => {
-              alert('Signed up')
-              this.segments = 'login';
-              this.loader.dismiss();
-          }, (err: IDetailedError<string[]>) => {
-              for (let e of err.details) {
-                  if (e === 'conflict_email') {
-                      alert('Email already exists.');
-                  } else {
-                      alert(e);
-                  }
-              }
-              this.loader.dismiss();
-          });
-      }
-
-      login(value: string) {
-          this.loader.present();
-          console.log(`Submitted values : email = ${value.email}, password = ${value.password}`);
-          let details: UserDetails = {'email': value.email, 'password': value.password};
-          this.auth.login('basic', details).then( () =>{
-              this.loader.dismiss();
-              this.navCtrl.setRoot(HomePage);
-          });
-
-      }
-
-      loginWithGoogle() {
-          this.auth.login('google').then( () =>{
-              alert('Logged in with Google');
-              this.navCtrl.setRoot(HomePage);
-          });
-      }
+    loginWithGoogle() {
+      this.auth.login('google').then( () =>{
+        alert('Logged in with Google');
+        this.navCtrl.setRoot(HomePage);
+      });
+    }
 
   }
