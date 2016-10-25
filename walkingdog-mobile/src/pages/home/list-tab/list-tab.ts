a2de79453d75214f7f95b4a84e17ab7442a1d26a
Paolo <pao.esco@outlook.com>
2016-10-25 18:29:25 +0100

    Closes #16. Start a walk button added.

--- walkingdog-mobile/src/pages/home/list-tab/list-tab.ts
+++ walkingdog-mobile/src/pages/home/list-tab/list-tab.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
 import { LoadingController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 import { Dog } from '../../../components/dog/dog.component';
 import { LocationTracker } from '../../../components/location/location-tracker';
 
@@ -18,7 +19,8 @@ export class ListTab {
   constructor(
     public navCtrl: NavController,
     public loadingCtrl: LoadingController,
-    public locationTracker: LocationTracker) {
+    public locationTracker: LocationTracker,
+    public toastCtrl: ToastController) {
     
   }
 
@@ -45,7 +47,16 @@ export class ListTab {
   }
 
   startWalk() {
+    let startWalkToast = this.toastCtrl.create({
+      message: 'You are now out for a walk. Other walkers are now able to follow you and ping you.',
+      duration: 8000,
+      position: 'top'
+    });
+    startWalkToast.present();
+
     this.locationTracker.startTracking();
+
+    // Stop tracking after 20 minutes.
     setTimeout(() => {
       this.locationTracker.stopTracking();
     }, 1000*60*20);
@@ -55,7 +66,7 @@ export class ListTab {
     this.dogs = new Array();
     let hyumiko: Dog = new Dog('Hyumiko','Acting like a cat. But can bark.', 'assets/dogs/shiba.jpg');
     this.dogs.push(hyumiko);
-    let reveur: Dog = new Dog('Reveur','Acting like a carpet.', 'assets/dogs/teckel.jpg');
+    let reveur: Dog = new Dog('Reveur','Acting like a carpet. Jean eater.', 'assets/dogs/teckel.jpg');
     this.dogs.push(reveur);
   }
 
