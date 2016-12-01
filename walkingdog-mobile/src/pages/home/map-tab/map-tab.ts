bde6631b8019a081215237d677250473daa84659
Paolo Escobar <pao.esco@outlook.com>
2016-12-01 15:10:40 +0000

    Adds call to backend to show nearest dogs on map, with markers

--- walkingdog-mobile/src/pages/home/map-tab/map-tab.ts
+++ walkingdog-mobile/src/pages/home/map-tab/map-tab.ts
@@ -1,6 +1,8 @@
 import { Component,ViewChild,ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LocationTracker } from '../../../components/location/location-tracker';
+import {Http, Response} from '@angular/http';
+
 
 // Comes from Google Maps JavaScript API. See index.html
 declare var google;
@@ -11,14 +13,23 @@ declare var google;
 })
 export class MapTab {
 
+  // Stores Google map
   map: any;
+  // Html element map to display Google map
   @ViewChild('map') mapElement: ElementRef;
+  // Stores the marker of the current user
   currentUserMarker: any;
+  // Stores the markers of pets around
+  petsAroundMarkers: Array<any>;
+
+  private apiUrl: String;
 
   constructor(
     public navCtrl: NavController, 
-    public locationTracker: LocationTracker) {
-
+    public locationTracker: LocationTracker,
+    private http: Http) {
+    this.apiUrl = 'https://walkingdog-services.herokuapp.com/api/map/dogsAround';
+    this.petsAroundMarkers = [];
   }
   
   // http://www.joshmorony.com/ionic-2-how-to-use-google-maps-geolocation-video-tutorial/
@@ -44,7 +55,11 @@ export class MapTab {
         this.currentUserMarker.setMap(null);
       }
       // Creates and stores a new marker with the current position.
-      this.currentUserMarker = this.addMarker("you", "You", this.locationTracker.lat, this.locationTracker.lng);
+      this.currentUserMarker = this.addMarker("you", "You", this.locationTracker.lat, this.locationTracker.lng, null);
+
+      // Find dogs around
+      this.showNearestPets();
+
     }, 10000);
   }
 
@@ -71,15 +86,16 @@ export class MapTab {
     // Centering the map on the user current position.
     let currentPosition = new google.maps.LatLng(this.locationTracker.lat, this.locationTracker.lng);
     this.map.setCenter(currentPosition);
-    this.currentUserMarker = this.addMarker("you", "You", this.locationTracker.lat, this.locationTracker.lng);
+    this.currentUserMarker = this.addMarker("you", "You", this.locationTracker.lat, this.locationTracker.lng, null);
   }
 
-  addMarker(dogId: string, dogName: string, lat: number, lng : number): any {
+  private addMarker(dogId: string, dogName: string, lat: number, lng : number, icon: string): any {
     let currentPosition = {lat: lat, lng: lng};
     let marker = new google.maps.Marker({
       map: this.map,
       animation: google.maps.Animation.DROP,
-      position: currentPosition
+      position: currentPosition,
+      icon: icon
     });
     let content = `<h4>${dogName}</h4>`;          
     this.addInfoWindow(marker, content);
@@ -93,6 +109,34 @@ export class MapTab {
     google.maps.event.addListener(marker, 'click', () => {
       infoWindow.open(this.map, marker);
     });
+  }
+
+  private showNearestPets() {
+    // Prepares the request
+    let params: string = [
+      `ne-lat=${this.map.getBounds().getNorthEast().lat()}`,
+      `ne-lon=${this.map.getBounds().getNorthEast().lng()}`,
+      `sw-lat=${this.map.getBounds().getSouthWest().lat()}`,
+      `sw-lon=${this.map.getBounds().getSouthWest().lng()}`
+    ].join('&');
+    // Sends request to backend
+    this.http.request(`${this.apiUrl}?${params}`)
+      .subscribe((res: Response) => {
+        console.log(res.json());
+        // Removes previous markers
+        for (let marker of this.petsAroundMarkers) {
+          marker.setMap(null);
+        }
+        // Clears array
+        this.petsAroundMarkers = []; 
+        // Creates new markers
+        for (let pet of res.json()) {
+          // Adds new marker
+          let marker = this.addMarker(pet.id, pet.name, pet.latitude, pet.longitude, 'assets/icon/pets.png');
+          // Keeps reference of created marker
+          this.petsAroundMarkers.push(marker);
+        }
+      });
   }
 
 }
