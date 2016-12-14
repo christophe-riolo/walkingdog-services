6801fe0fcd60c09a23a064f0148f9eebb78187e7
Paolo Escobar <pao.esco@outlook.com>
2016-12-14 11:11:38 +0000

    Cors for Post

--- walkingdog-services/src/main/java/com/hubesco/software/walkingdog/services/RouterSingleton.java
+++ walkingdog-services/src/main/java/com/hubesco/software/walkingdog/services/RouterSingleton.java
@@ -5,6 +5,8 @@ import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.CorsHandler;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -16,8 +18,13 @@ public class RouterSingleton {
 
     public synchronized static Router router(Vertx vertx) {
         if (router == null) {
+            Set<HttpMethod> methods = new HashSet<>();
+            methods.add(HttpMethod.GET);
+            methods.add(HttpMethod.POST);
             router = Router.router(vertx);
-            CorsHandler cors = CorsHandler.create("*").allowedMethod(HttpMethod.GET);
+            CorsHandler cors = CorsHandler
+                    .create("*")
+                    .allowedMethods(methods);
             router.route().handler(cors);
             router.route().handler(BodyHandler.create());
         }
