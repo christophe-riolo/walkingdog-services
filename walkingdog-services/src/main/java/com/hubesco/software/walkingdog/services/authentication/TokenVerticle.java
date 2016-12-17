4c318603cd064754560528ebae6240e6da5f8c59
Paolo Escobar <pao.esco@outlook.com>
2016-12-17 00:46:20 +0000

    Fixes tests and keystore password

--- walkingdog-services/src/main/java/com/hubesco/software/walkingdog/services/authentication/TokenVerticle.java
+++ walkingdog-services/src/main/java/com/hubesco/software/walkingdog/services/authentication/TokenVerticle.java
@@ -25,7 +25,7 @@ public class TokenVerticle extends AbstractVerticle {
             JsonObject config = new JsonObject().put("keyStore", new JsonObject()
                     .put("path", EnvironmentProperties.jwtKeystorePath())
                     .put("type", "jceks")
-                    .put("password", EnvironmentProperties.jwtKeystoreKeypass()));
+                    .put("password", EnvironmentProperties.jwtKeystoreStorepass()));
             provider = JWTAuth.create(vertx, config);
             vertx.eventBus().consumer(Addresses.TOKEN.address(), this::handler);
             startFuture.complete();
